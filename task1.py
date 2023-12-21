aeb6f189faf13042a08490c394504b3c77b218a8
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-12-21 12:35:31 -0800

    update

--- task1.py
+++ task1.py
@@ -12,6 +12,8 @@ def play_shsound():
     ps.playsound('sound-sheep.mp3')
 def play_dosound():
     ps.playsound('bray-donkey.mp3')
+def play_husound():
+    ps.playsound('screaming-huggus.mp3')
 
 cow_img = PhotoImage(file='cow300.png')
 b_cow = Button(win,image=cow_img, command=play_cosound)
@@ -21,12 +23,14 @@ sheep_img = PhotoImage(file='sheep300.png')
 b_sheep = Button(win,image=sheep_img, command=play_shsound)
 donkey_img = PhotoImage(file='donkey300.png')
 b_donkey = Button(win,image=donkey_img, command=play_dosound)
-bluemonster_img = PhotoImage(win,file='')
-b_bluemonster= Button(win,text='More...')
+huggus_img = PhotoImage(win,file='huggus300.png')
+b_huggus = Button(win,image=huggus_img, command=play_husound)
 
 b_cow.grid(row=1,column=1)
 b_duck.grid(row=2,column=1)
 b_sheep.grid(row=3,column=1)
 b_donkey.grid(row=1,column=2)
+
+b_huggus.grid(row=3,column=2)
 
 win.mainloop()
